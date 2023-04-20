8decf8387c81ee16140638c2f0c6c13ea08fc1a7
lucas.rech <lucas.rech@cwi.com.br>
2023-04-20 20:44:10 -0300

    Inicialização do projeto.

---
+++ src/test/java/com/poc/pubsub/PubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.pubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
